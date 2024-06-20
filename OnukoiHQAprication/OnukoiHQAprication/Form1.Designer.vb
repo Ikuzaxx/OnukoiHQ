ec33af9914950547166d298094562608811a874e
麻婆豆腐 四川風 <yurag@outlook.jp>
2024-06-20 20:19:20 +0900

    ログインUIの調整

--- OnukoiHQAprication/OnukoiHQAprication/Form1.Designer.vb
+++ OnukoiHQAprication/OnukoiHQAprication/Form1.Designer.vb
@@ -35,48 +35,50 @@ Partial Class Form1
         Me.Label1.AutoSize = True
         Me.Label1.Font = New System.Drawing.Font("MS UI Gothic", 20.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(128, Byte))
         Me.Label1.ForeColor = System.Drawing.Color.FromArgb(CType(CType(255, Byte), Integer), CType(CType(128, Byte), Integer), CType(CType(128, Byte), Integer))
-        Me.Label1.Location = New System.Drawing.Point(-2, 9)
-        Me.Label1.Margin = New System.Windows.Forms.Padding(1, 0, 1, 0)
+        Me.Label1.Location = New System.Drawing.Point(32, 9)
+        Me.Label1.Margin = New System.Windows.Forms.Padding(2, 0, 2, 0)
         Me.Label1.Name = "Label1"
-        Me.Label1.Size = New System.Drawing.Size(374, 27)
+        Me.Label1.Size = New System.Drawing.Size(745, 54)
         Me.Label1.TabIndex = 0
         Me.Label1.Text = "虹ノ未来学園生徒会データベース"
         '
         'Button1
         '
         Me.Button1.Font = New System.Drawing.Font("MS UI Gothic", 15.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(128, Byte))
-        Me.Button1.Location = New System.Drawing.Point(44, 248)
-        Me.Button1.Margin = New System.Windows.Forms.Padding(1, 2, 1, 2)
+        Me.Button1.Location = New System.Drawing.Point(95, 496)
+        Me.Button1.Margin = New System.Windows.Forms.Padding(2, 4, 2, 4)
         Me.Button1.Name = "Button1"
-        Me.Button1.Size = New System.Drawing.Size(288, 87)
+        Me.Button1.Size = New System.Drawing.Size(624, 174)
         Me.Button1.TabIndex = 1
         Me.Button1.Text = "ログイン"
         Me.Button1.UseVisualStyleBackColor = True
         '
         'TextBox1
         '
-        Me.TextBox1.Location = New System.Drawing.Point(44, 200)
-        Me.TextBox1.Margin = New System.Windows.Forms.Padding(1, 2, 1, 2)
+        Me.TextBox1.Location = New System.Drawing.Point(95, 400)
+        Me.TextBox1.Margin = New System.Windows.Forms.Padding(2, 4, 2, 4)
         Me.TextBox1.Multiline = True
         Me.TextBox1.Name = "TextBox1"
-        Me.TextBox1.Size = New System.Drawing.Size(290, 34)
+        Me.TextBox1.Size = New System.Drawing.Size(624, 64)
         Me.TextBox1.TabIndex = 2
         '
         'TextBox2
         '
-        Me.TextBox2.Location = New System.Drawing.Point(44, 125)
+        Me.TextBox2.Location = New System.Drawing.Point(95, 250)
+        Me.TextBox2.Margin = New System.Windows.Forms.Padding(6, 6, 6, 6)
         Me.TextBox2.Multiline = True
         Me.TextBox2.Name = "TextBox2"
-        Me.TextBox2.Size = New System.Drawing.Size(290, 34)
+        Me.TextBox2.Size = New System.Drawing.Size(624, 64)
         Me.TextBox2.TabIndex = 3
         '
         'Label2
         '
         Me.Label2.AutoSize = True
         Me.Label2.Font = New System.Drawing.Font("MS UI Gothic", 20.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(128, Byte))
-        Me.Label2.Location = New System.Drawing.Point(44, 83)
+        Me.Label2.Location = New System.Drawing.Point(95, 166)
+        Me.Label2.Margin = New System.Windows.Forms.Padding(6, 0, 6, 0)
         Me.Label2.Name = "Label2"
-        Me.Label2.Size = New System.Drawing.Size(97, 27)
+        Me.Label2.Size = New System.Drawing.Size(193, 54)
         Me.Label2.TabIndex = 4
         Me.Label2.Text = "User ID"
         '
@@ -84,25 +86,26 @@ Partial Class Form1
         '
         Me.Label3.AutoSize = True
         Me.Label3.Font = New System.Drawing.Font("MS UI Gothic", 20.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(128, Byte))
-        Me.Label3.Location = New System.Drawing.Point(44, 171)
+        Me.Label3.Location = New System.Drawing.Point(95, 342)
+        Me.Label3.Margin = New System.Windows.Forms.Padding(6, 0, 6, 0)
         Me.Label3.Name = "Label3"
-        Me.Label3.Size = New System.Drawing.Size(130, 27)
+        Me.Label3.Size = New System.Drawing.Size(261, 54)
         Me.Label3.TabIndex = 5
         Me.Label3.Text = "Pass Word"
         '
         'Form1
         '
-        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 12.0!)
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(13.0!, 24.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.BackColor = System.Drawing.Color.White
-        Me.ClientSize = New System.Drawing.Size(373, 342)
+        Me.ClientSize = New System.Drawing.Size(808, 684)
         Me.Controls.Add(Me.Label3)
         Me.Controls.Add(Me.Label2)
         Me.Controls.Add(Me.TextBox2)
         Me.Controls.Add(Me.TextBox1)
         Me.Controls.Add(Me.Button1)
         Me.Controls.Add(Me.Label1)
-        Me.Margin = New System.Windows.Forms.Padding(1, 2, 1, 2)
+        Me.Margin = New System.Windows.Forms.Padding(2, 4, 2, 4)
         Me.Name = "Form1"
         Me.Text = "QuestionHQ(虹ノ学園生徒会データベース)"
         Me.ResumeLayout(False)
